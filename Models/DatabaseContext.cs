297b0b5ab9a3ef153d2729de6fb7751306eafae2
joshuackeller <68955051+joshuackeller@users.noreply.github.com>
2022-01-31 18:04:03 -0700

    create project

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace JoelHilton.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext (DbContextOptions<DatabaseContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<AddMovieModel> responses { get; set; }
+    }
+}
